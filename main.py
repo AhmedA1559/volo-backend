26b9995bf95bb1b10045cc5e15a2ba86d0e67caf
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2022-09-23 21:22:26 -0500

    Add firebase

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from flask import Flask, jsonify
+from firebase_admin import credentials, firestore, initialize_app
 import os
 
 app = Flask(__name__)
 
+initialize_app(
+    credential=credentials.Certificate({
+        "type": "service_account",
+        "project_id": os.environ.get('FIREBASE_PROJECT_ID'),
+        "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
+        "private_key": os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+        "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
+        "client_id": os.environ.get('CLIENT_ID'),
+        "auth_uri": os.environ.get('AUTH_URI'),
+        "token_uri": os.environ.get('TOKEN_URI'),
+        "auth_provider_x509_cert_url": os.environ.get('AUTH_PROVIDER_X509_CERT_URL'),
+        "client_x509_cert_url": os.environ.get('CLIENT_X509_CERT_URL')}
+    )
+)
+
+db = firestore.client()
+db_ref = db.collection('events')
+
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    db_ref.document('test').set({'test': 'test'})
+    return jsonify({"payload": db_ref.document('test').get().to_dict()}), 200
 
 
 if __name__ == '__main__':
